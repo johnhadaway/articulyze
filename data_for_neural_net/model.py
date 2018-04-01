b0612e04973b83c2fecd4707908aa12656704898
Na Chaichanawanich <nanut@cs.uchicago.edu>
2018-04-01 00:00:08 -0500

    Saved Model

--- data_for_neural_net/model.py
+++ data_for_neural_net/model.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
@@ -26,3 +27,11 @@ model.fit(X, Y, epochs=300, batch_size=10)
 # evaluate the model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
