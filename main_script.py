95033d384fb2743357c1168aa1a0269c64b49465
johnhadaway <johnhadaway8@gmail.com>
2018-04-13 20:48:57 -0500

    updates yadn

--- main_script.py
+++ main_script.py
@@ -7,6 +7,7 @@
 import ranking_a as ranker
 import news_scraping as scraper
 import pandas as pd
+import sqlite3 as sqlite
 
 search_countries = ["afghanistan", "burundi", "cameroon", "central+african+republic", "chad", "democratic+republic+of+the+congo", "ethiopia", "haiti", "iraq", "libya", "mali",
     "myanmar", "niger", "nigeria", "palestine", "somalia", "south+sudan", "sudan", "syria", "ukraine", "yemen"]
@@ -21,6 +22,8 @@ def extract_country_from_file_name(file_name):
 
 def daily_script(): 
 
+	# delete all files in data\countries directory 
+
     # generate search queries
     search_queries = scraper.generate_search_queries(search_countries, additional_queries)
 
@@ -29,16 +32,24 @@ def daily_script():
 
     ranked_dictionaries = {}
 
+    # go through all file names and create dictionary mapping country name to ranked dictionary
     for file_name in file_names:
-        print(file_name)
         ranked_dictionary = ranker.ranking(file_name)
         country_name = extract_country_from_file_name(file_name)
         ranked_dictionaries.update({country_name : ranked_dictionary})
 
-    # store all of the dictionaries into sql database 
-    # -> remove items from more than 7 days from database, into archive
+    # connect to database (or create on first run), and create cursor object for it 
+    conn = sqlite.connect("website" + "\\" + "current_unohca.db")
+    cursor = conn.cursor()
+
+    # update / create tables for each country in the database (NOT DONE)
+    for key in ranked_dictionaries: 
+    	ranked_dictionary = ranked_dictionaries[key]
+
+    # remove articles that have been there for more than 7 days and store them in an archive.db
+
+    # delete duplicates in both archive and current 
+    
     # re-sort all databases by score 
 
     return None
-
-daily_script()
