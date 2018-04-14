6e444a8d6731dc7de4c1a0041a3cd415943d4d92
johnhadaway <johnhadaway8@gmail.com>
2018-04-14 01:54:09 -0500

    updates

--- main_script.py
+++ main_script.py
@@ -8,9 +8,12 @@ import ranking_a as ranker
 import news_scraping as scraper
 import pandas as pd
 import sqlite3 as sqlite
+import os
 
 search_countries = ["afghanistan", "burundi", "cameroon", "central+african+republic", "chad", "democratic+republic+of+the+congo", "ethiopia", "haiti", "iraq", "libya", "mali",
     "myanmar", "niger", "nigeria", "palestine", "somalia", "south+sudan", "sudan", "syria", "ukraine", "yemen"]
+
+test_countries = ["afghanistan", "burundi"]
 additional_queries = ["", "crisis", "humanitarian"]
 
 def extract_country_from_file_name(file_name):
@@ -22,34 +25,35 @@ def extract_country_from_file_name(file_name):
 
 def daily_script(): 
 
-	# delete all files in data\countries directory 
+    # delete all files in data\countries directory 
+    fileList = os.listdir("data/countries")
+    for fileName in fileList:
+        os.remove("data/countries/"+fileName)
 
     # generate search queries
-    search_queries = scraper.generate_search_queries(search_countries, additional_queries)
+    search_queries = scraper.generate_search_queries(test_countries, additional_queries)
 
     # update all / return file names
     file_names = scraper.update_all(search_queries)
 
-    ranked_dictionaries = {}
+    country_to_articles_dic = {}
 
-    # go through all file names and create dictionary mapping country name to ranked dictionary
+    # go through all file names and create dictionary mapping country name to ranked list of lists
     for file_name in file_names:
-        ranked_dictionary = ranker.ranking(file_name)
+        ranked_list_of_lists = ranker.ranking(file_name)
         country_name = extract_country_from_file_name(file_name)
-        ranked_dictionaries.update({country_name : ranked_dictionary})
+        country_to_articles_dic.update({country_name : ranked_list_of_lists})
 
-    # connect to database (or create on first run), and create cursor object for it 
-    conn = sqlite.connect("website" + "\\" + "current_unohca.db")
-    cursor = conn.cursor()
+    # connect to current_database (or create on first run), and create cursor object for it 
+    conn_current = sqlite.connect("website\current_unohca.db")
+    cursor_current = conn_current.cursor()
 
     # update / create tables for each country in the database (NOT DONE)
-    for key in ranked_dictionaries: 
-    	ranked_dictionary = ranked_dictionaries[key]
 
-    # remove articles that have been there for more than 7 days and store them in an archive.db
+    # remove articles that have been there for more than 7 days and store them in an archive.db / delete duplicates in archive.db 
 
-    # delete duplicates in both archive and current 
-    
-    # re-sort all databases by score 
+    # delete duplicates in current tables and re-sort all tables by score 
 
     return None
+
+daily_script()
